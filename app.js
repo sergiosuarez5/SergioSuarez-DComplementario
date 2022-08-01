76866671b9e87530373826904838b6655e35111a
“sergio <sergiowork98@gmail.com>
2022-08-01 18:30:25 -0300

    desafio 2 modifico funciones, html y css Prestamo

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-/* POR SERGIO SUAREZ
+// POR SERGIO SUAREZ
 
-    Este código consiste en ir preguntando mediante un prompt, cuales son los jugadores de un equipo.
+// Funcion
 
-    Variables que se usarán: 
-        listaDeJugadores, es un string donde se irán anexando los invitados.
-        numeroDeJugador, que indica el órden en el que se ingresaron.
-        jugador, que es la variable que va a irse utilizando para ir anexando los jugadores.
-*/
-let listaDeJugadores = "";
-let numeroDeJugador = 1;
-let jugador = "";
+let cliente = prompt("Bienvenido, Ingrese su nombre para continuar");
 
-// Alertas indicando cómo ingresar los datos
-alert("REGISTRO DE INVITACIÓN A TORNEO")
-alert("A continuación, escribe el nombre de cada uno de tus Jugadores, y finaliza pulsando la tecla 'Esc' o 'Cancelar'");
-
-// Decidí el Ciclo do, para que se preguntara por al menos un jugador
-do {
-    jugador = prompt("Jugador #" + numeroDeJugador + ":                (Pulsa 'Esc' o Cancelar para Finalizar)");
-    
-    // La condición es que no se presione esc y que el jugador tenga al menos 1 caracter
-    if (jugador != null && jugador != "") {
-        listaDeJugadores += numeroDeJugador + ". " + jugador + "\n";
-        console.log("Se registró al jugador "+numeroDeJugador+", el cual se llama "+jugador);
-        numeroDeJugador++;
-    } else {
-        console.log("No se hizo un registro adicional");
+function edadInvalida (edad){
+    if((edad ="") && (edad = null)){
+    alert("Error: Ingrese caracteres numericos");
+    return (true);
+    }else {
+        return (false);
     }
+}
+do{
+    edad = parseInt(prompt ("Ingrese su edad."));
+}while (edadInvalida(edad));
 
-} 
-// El bucle termina cuando se presiona la tecla Esc y por tanto el jugador es null
-while (jugador != null);
+alert("Bienvenido " + cliente + "!");
+let dineroSolicitado = prompt ("Ingrese el importe que le gustaria obtener. (sin ceros, ni comas)")
+let imporSolicitado = parseInt(dineroSolicitado);
+alert("Dinero solicitado: " + dineroSolicitado + "\nHaz click en Aceptar si el valor es correcto, si no esta de acuerdo recargue nuevamente la pagina.")
+let cuotas = prompt("Ingrese la cantidad de cuotas que desea. \nTeniendo en cuenta un 6% de intereses.")
+let cuotasPagar= parseInt(cuotas);
+let interesMensual = imporSolicitado * 0.06;
+let interesTotal = interesMensual * cuotasPagar;
 
-// Finalización del código
-console.log("Finaliza el registro de jugadores. En total fueron " + (numeroDeJugador-1))
-if (listaDeJugadores != "") alert("LOS JUGADORES SON:\n" + listaDeJugadores)
-else alert("No hay jugadores");
+function totalPrestamo() {
+    resultado = imporSolicitado + interesTotal;
+    return(resultado);
+}
+totalPrestamo();
+alert("Dinero solicitado: " + dineroSolicitado + "\nCuotas a pagar: " + cuotas + "\nIntereses totales: " + interesTotal)
+alert ("Gracias por confiar en nosotros " + cliente) + "!";
